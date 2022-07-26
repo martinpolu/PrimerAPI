7b5cdfd13302a6eb940ad3e0b749f866e1d4f232
martinpolu <martinpolu@gmail.com>
2022-07-26 19:09:24 -0300

    Se utilizan datos de infoclima para poder utilizar web scraping.

---
+++ CodigoCiudades.py
@@ -0,0 +1,5 @@
+Ciudades={"COR":"https://infoclima.com/pronosticos/argentina/cordoba/cordoba/",
+          "BUE":"https://infoclima.com/pronosticos/argentina/capital-federal/buenos-aires/",
+          "SJ":"https://infoclima.com/pronosticos/argentina/san-juan/san-juan/",
+          "SL":"https://infoclima.com/pronosticos/argentina/san-luis/san-luis/",
+          "MDQ":"https://infoclima.com/pronosticos/argentina/buenos-aires/mar-del-plata/"}
--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import requests
 import json
+from bs4 import BeautifulSoup
+from CodigoCiudades import Ciudades
 
 
-x = requests.get('https://ws.smn.gob.ar/map_items/weather')
-# convert dictionary string to dictionary
-print(x.text)
-res = json.loads(x.text)
+def ObtenerTemperatura(Ciudad):
+    x = requests.get(Ciudad) #Hacemos el get para obtener el json completo
+    #print(x.text)
+    soup = BeautifulSoup(x.text, "html.parser")
+    TablaTemperatura = soup.find_all("div", {"class": "d1"})
+    Temperatura=TablaTemperatura[1].text.split()[0][:-2] #Obtenemos temperatura sacandole el "ºC"
+    TablaRegistro = soup.find_all("div",{"class":"nota1 ea"})
+    HoraRegistro=(TablaRegistro[0].text.split()[3])
+    return({"Temperatura":Temperatura,"Horario":HoraRegistro})
+
+def ObtenerHumedad(Ciudad):
+    x = requests.get(Ciudad) #Hacemos el get para obtener el json completo
+    #print(x.text)
+    soup = BeautifulSoup(x.text, "html.parser")
+    TablaHumedad = soup.find_all("div", {"class": "d1"})
+    Humedad=TablaHumedad[2].text.split()[1][:-1] #Obtenemos presion sacandole el "%"
+    TablaRegistro = soup.find_all("div",{"class":"nota1 ea"})
+    HoraRegistro=(TablaRegistro[0].text.split()[3])
+    return({"Humedad":Humedad,"Horario":HoraRegistro})
+
+def ObtenerPresion(Ciudad):
+    x = requests.get(Ciudad) #Hacemos el get para obtener el json completo
+    #print(x.text)
+    soup = BeautifulSoup(x.text, "html.parser")
+    TablaPresion = soup.find_all("div", {"class": "d1"})
+    Presion=TablaPresion[2].text.split()[3] #Obtenemos temperatura sacandole el "ºC"
+    TablaRegistro = soup.find_all("div",{"class":"nota1 ea"})
+    HoraRegistro=(TablaRegistro[0].text.split()[3])
+    return({"Presion":Presion,"Horario":HoraRegistro})
+
+
+
+print(ObtenerPresion(Ciudades["MDQ"]))
+print(ObtenerTemperatura(Ciudades["MDQ"]))
+print(ObtenerHumedad(Ciudades["MDQ"]))
