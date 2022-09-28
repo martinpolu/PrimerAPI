025bcda5c1486a3ebd4abe82579c05a6a97dced9
martinpolu <martinpolu@gmail.com>
2022-09-28 19:56:12 -0300

    Se agregó el archivo requeriments.

--- RequestAPI.py
+++ RequestAPI.py
@@ -3,19 +3,21 @@ import json
 import time
 from pymongo import MongoClient
 from CodigoCiudades import ListCities
+import datetime  
 
 # This script make a request to localhost to obtain values of temp, hum and pres.
 # After obtaining them, it uploads them to a database
 
-def ConsultarFechaUltimoDato():
+def ConsultarFechaUltimoDato(CodCity):
     client = MongoClient()
     DataBase = client.clima
     DocClima = DataBase.clima
-    result=(DocClima.find().sort('_id',-1).limit(1)) #Find the last item in db.
-    if result.retrieved==0: #If doesn´t exist values, return 0
-        return 0
-    else:
-        return(result["Horario"])
+    result = (DocClima.find({"Ciudad": CodCity}, {}).sort('_id', -1).limit(1)) #Find the last item in db.
+    for kas in result:
+        print(kas)
+        print(datetime.datetime.fromtimestamp( kas["Horario"] )  )
+        return(str(kas["Horario"]))
+    return 0
 
 
 
@@ -23,20 +25,19 @@ while(True):
     for Codigos in ListCities:
         r = requests.get("http://127.0.0.1:50000/Temperatura/?CodCiu="+Codigos)
         res = json.loads(r.text) #Convert text to dict.
-        UltimoHorario=ConsultarFechaUltimoDato()
+        UltimoHorario=ConsultarFechaUltimoDato(Codigos)
         if res["Horario"]!=UltimoHorario:
             datos={'Ciudad':res["Ciudad"],'Dato':'Temperatura','Valor':res["Temperatura"],"Horario":res["Horario"]}
             r = requests.post("http://127.0.0.1:50000/CargarDatos/",data=datos)
-            print(r.text)
-            r = requests.get("http://127.0.0.1:50000/Humedad/?CodCiu=COR")
+            r = requests.get("http://127.0.0.1:50000/Humedad/?CodCiu="+Codigos)
             res = json.loads(r.text)
             datos={'Ciudad':res["Ciudad"],'Dato':'Humedad','Valor':res["Humedad"],"Horario":res["Horario"]}
             r = requests.post("http://127.0.0.1:50000/CargarDatos/",data=datos)
-            r = requests.get("http://127.0.0.1:50000/Presion/?CodCiu=COR")
+            r = requests.get("http://127.0.0.1:50000/Presion/?CodCiu="+Codigos)
             res = json.loads(r.text)
             datos={'Ciudad':res["Ciudad"],'Dato':'Presion','Valor':res["Presion"],"Horario":res["Horario"]}
             r = requests.post("http://127.0.0.1:50000/CargarDatos/",data=datos)
         else:
             print("mismo dato")
-    time.sleep(3600)
+    time.sleep(120)
 
